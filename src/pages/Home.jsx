08f5d1fdd85d37dbf5a158ae37cbcf194fc5871a
julianta <bis4.dev@gmail.com>
2023-12-18 16:08:06 +0800

    feat: add react-router and implementation atomic design

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl underline">Hello World</h1>
+    </div>
+  );
+}
